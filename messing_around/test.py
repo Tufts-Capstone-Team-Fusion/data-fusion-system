f8c6f8fbae046ad95cf1c9d1e046538f922b099d
bohnsackcharlie <Bohnsackcharlie@gmail.com>
2024-02-25 17:13:47 -0500

    calibration

--- messing_around/test.py
+++ messing_around/test.py
@@ -29,7 +29,7 @@ image2 = cv2.imread("image2small.png")
 # Manually input corresponding points
 # used photpea to find points
 # src is thermal
-src_pts = [
+therm_pts = [
     [238, 160],
     [264, 164],
     [247, 223], 
@@ -38,7 +38,7 @@ src_pts = [
     [317, 506]
 ]
 # dst is visual
-dst_pts = [
+vis_pts = [
     [320, 89],
     [357, 94],
     [332, 174],
@@ -48,8 +48,8 @@ dst_pts = [
 ]
 
 # Convert points to numpy arrays
-src_pts = np.array(src_pts).reshape(-1, 1, 2)
-dst_pts = np.array(dst_pts).reshape(-1, 1, 2)
+dst_pts = np.array(therm_pts).reshape(-1, 1, 2)
+src_pts = np.array(vis_pts).reshape(-1, 1, 2)
 
 
 # Compute homography matrix using RANSAC
@@ -57,24 +57,38 @@ homography_matrix, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 
 print(homography_matrix)
 
-x = 317
-y = 506
+def translatePoint(x, y):
+    pt = np.array([[x], [y], [1]], dtype=np.float32)
+    transformed_pt = np.dot(homography_matrix, pt)
 
-pt = np.array([[x], [y], [1]], dtype=np.float32)
+    w = transformed_pt[2, 0]
+    u = transformed_pt[0, 0] / w
+    v = transformed_pt[1, 0] / w
 
-transformed_pt = np.dot(homography_matrix, pt)
+    # print(f"Transformed Point: ({u}, {v})")
 
-w = transformed_pt[2, 0]
-u = transformed_pt[0, 0] / w
-v = transformed_pt[1, 0] / w
+    return (round(u), round(v))
 
-print(f"Transformed Point: ({u}, {v})")
+
 # Use homography matrix for transformation if needed
-warped_image = cv2.warpPerspective(image1, homography_matrix, (image2.shape[1], image2.shape[0]))
+# warped_image = cv2.warpPerspective(image1, homography_matrix, (image2.shape[1], image2.shape[0]))
+
+# Get the dimensions of the image
+height, width = image1.shape[:2]
+
+# Loop over all points in the image
+for y in range(height):
+    for x in range(width):
+        tX, tY = translatePoint(x, y)
+        image2[tY, tX] = image1[y, x]
+        # for c in range(image1.shape[2]):
+        #     image2[tX, tY, c] = image1[x, y, c]
+
+
 
 # Display the images side by side
 # concatenated_images = np.concatenate((image1, image2), axis=1)
-cv2.imshow("Matches", warped_image)
+cv2.imshow("Matches", image2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
