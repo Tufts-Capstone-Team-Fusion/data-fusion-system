293ab9987af5f3da34d403a3b9e244b5a70e2625
Matt Holsten <m.holsten3@gmail.com>
2024-02-26 15:30:51 -0500

    add: calibration.py

---
+++ code/calibration.py
@@ -0,0 +1,9 @@
+'''
+
+Link to FLIR website talking about calibration (including what OpenCV links to use)
+https://flir.custhelp.com/app/answers/detail/a_id/4639/~/flir-oem---boson%2C-lepton%2C-or-tau---intrinsic-thermal-camera-core-calibration#:~:text=The%20calibration%20can%20be%20done,halogen%20lamp%2C
+
+Link to chessboard imaging help from OpenCV stereo camera calibration
+https://martinperis.blogspot.com/2011/01/opencv-stereo-camera-calibration.html
+
+'''
